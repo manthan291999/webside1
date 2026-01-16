433d1dc38d70e8020a758d7343c93aa0cef76cef
Manthan Mittal <manthanmittal93@gmail.com>
2026-01-16 19:00:29 +0530

    Clean up unused files and add loading screen

--- src/components/canvas/index.ts
+++ src/components/canvas/index.ts
@@ -1,7 +1,6 @@
 // Canvas Components Export
 export { default as CanvasLoader } from "./CanvasLoader";
 
-export { default as SkillSpheres } from "./SkillSpheres";
 export { default as DigitalEarth } from "./DigitalEarth";
 export { default as NeuralNetwork3D } from "./NeuralNetwork3D";
 export { default as ProjectsBackground3D } from "./ProjectsBackground3D";
